92e0d107b8944f9d4d9fbd76bd90032e0bbb2646
TU. CHENG SHIN <11463469+tutumomo@users.noreply.github.com>
2023-12-13 14:27:35 +0800

    Update lua.lua

--- languages/lua.lua
+++ languages/lua.lua
@@ -8,14 +8,14 @@
 ---------------------------------------------------------------------------------
 --[[
 	Lua 特性：
-		轻量级：源码2.5万行左右C代码， 方便嵌入进宿主语言(C/C++)
-		可扩展：提供了易于使用的扩展接口和机制， 使用宿主语言提供的功能
-		高效性：运行最快的脚本语言之一
+		輕量級：源碼2.5萬行左右C代碼， 方便嵌入進宿主語言(C/C++)
+		可擴展：提供了易於使用的擴展接口和機制， 使用宿主語言提供的功能
+		高效性：運行最快的腳本語言之一
 		可移植：跨平台
-	入门书籍《lua程序设计》
-		推荐：云风翻译的《Lua 5.3参考手册》	
+	入門書籍《lua程序設計》
+		推薦：雲風翻譯的《Lua 5.3參考手冊》	
 		http://cloudwu.github.io/lua53doc/manual.html
-	源码： 
+	源碼： 
 		http://www.lua.org/ftp/
 --]]
 ---------------------------------------------------------------------------------
@@ -23,18 +23,18 @@
 
 ---------------------------------------------------------------------------------
 --[[
-	变量: 作为动态类型语言，变量本身没有类型， 赋值决定某一时刻变量的类型。私有静态
-	变量带local, 公有静态变量不带local。
-		数据类型：
-			nil            	为空，无效值，在条件判断中表示false
-			boolean			包含两个值：false和true
-			number			表示双精度类型的实浮点数
-			string			字符串由一对双引号或单引号来表示
-			function		由 C 或 Lua 编写的函数
-			table			Lua 中的表（table）其实是一个"关联数组"（associative
-							arrays），数组的索引可以是数字、字符串或表类型
-			thread			协程
-			userdata		存储在变量中的C数据结构
+	變量: 作為動態類型語言，變量本身沒有類型， 賦值決定某一時刻變量的類型。私有靜態
+	變量帶local, 公有靜態變量不帶local。
+		數據類型：
+			nil            	為空，無效值，在條件判斷中表示false
+			boolean			包含兩個值：false和true
+			number			表示雙精度類型的實浮點數
+			string			字符串由一對雙引號或單引號來表示
+			function		由 C 或 Lua 編寫的函數
+			table			Lua 中的表（table）其實是一個"關聯數組"（associative
+							arrays），數組的索引可以是數字、字符串或表類型
+			thread			協程
+			userdata		存儲在變量中的C數據結構
 --]]
 ---------------------------------------------------------------------------------
 print(type(signal))						--nil
@@ -88,19 +88,19 @@ do
 end
 
 --for
-for i=#ut_companys, 1, -2 do        --以2为步长反向遍历
+for i=#ut_companys, 1, -2 do        --以2為步長反向遍歷
     print("num: ", i, "company: ", ut_companys[i])
 end
 
 
 ---------------------------------------------------------------------------------
 --[[
-	table： 表作为Lua唯一自带的数据结构， 使用简单方便， 兼具数组和Map作为容器的
-	功能，通过表可以很容易组成常见的数据结构， 如栈、队列、链表、集合，用for循环
-	很容易迭代遍历表数据。
+	table： 表作為Lua唯一自帶的數據結構， 使用簡單方便， 兼具數組和Map作為容器的
+	功能，通過表可以很容易組成常見的數據結構， 如棧、隊列、鏈表、集合，用for循環
+	很容易迭代遍歷表數據。
 --]]
 ---------------------------------------------------------------------------------
---table当数组用，下标从1开始
+--table當數組用，下標從1開始
 for i, c in ipairs(ut_companys) do
 	print(string.format("1 UnionTech company: %d		%s", i, c))
 end
@@ -110,7 +110,7 @@ for i=#ut_companys, 1, -1 do
 	print(string.format("2 UnionTech company: %d		%s", i, ut_companys[i]))
 end
 
---table当hash map用
+--table當hash map用
 ut_cptypes = {}
 
 ut_cptypes["adapter"] = {"beijing company", "wuhan company", "guangzhou company"}
@@ -126,8 +126,8 @@ end
 
 ---------------------------------------------------------------------------------
 --[[
-	函数：在Lua中函数也是第一类型值， 可赋值给变量， 也可以在函数体内定义并使用函数，或者
-	是直接使用匿名匿名函数。
+	函數：在Lua中函數也是第一類型值， 可賦值給變量， 也可以在函數體內定義並使用函數，或者
+	是直接使用匿名匿名函數。
 --]]
 ---------------------------------------------------------------------------------
 --多重返回值
@@ -160,7 +160,7 @@ for i, ty in ipairs(types) do
 	end
 end 
 
---变参
+--變參
 function printf(str, ...)
 	print(string.format(str, ...))
 end
@@ -180,7 +180,7 @@ for i=1, #ut_companys do
 	printf("4 UnionTech company: %d		%s", i, ut_companys[i])
 end
 
---闭包
+--閉包
 function all_companys(cps)
 	local companys, n = {}, 0
 	for _, v in ipairs(cps) do
@@ -210,43 +210,43 @@ end
 
 ---------------------------------------------------------------------------------
 --[[
-	协程(coroutine)：Lua协同程序(coroutine)与线程比较类似：拥有独立的堆栈，独立的局
-	部变量，独立的指令指针，同时又与其它协同程序共享全局变量和其它大部分东西。
+	協程(coroutine)：Lua協同程序(coroutine)與線程比較類似：擁有獨立的堆棧，獨立的局
+	部變量，獨立的指令指針，同時又與其它協同程序共享全局變量和其它大部分東西。
 --]]
 ---------------------------------------------------------------------------------
 function foo (a)
-    print("foo 函数输出", a)
+    print("foo 函數輸出", a)
     return coroutine.yield(2 * a) -- 返回  2*a 的值
 end
  
 co = coroutine.create(function (a , b)
-    print("第一次协同程序执行输出", a, b) -- co-body 1 10
+    print("第一次協同程序執行輸出", a, b) -- co-body 1 10
     local r = foo(a + 1)
      
-    print("第二次协同程序执行输出", r)
-    local r, s = coroutine.yield(a + b, a - b)  -- a，b的值为第一次调用协同程序时传入
+    print("第二次協同程序執行輸出", r)
+    local r, s = coroutine.yield(a + b, a - b)  -- a，b的值為第一次調用協同程序時傳入
      
-    print("第三次协同程序执行输出", r, s)
-    return b, "结束协同程序"                   -- b的值为第二次调用协同程序时传入
+    print("第三次協同程序執行輸出", r, s)
+    return b, "結束協同程序"                   -- b的值為第二次調用協同程序時傳入
 end)
        
 print("main", coroutine.resume(co, 1, 10)) -- true, 4
 print("main", coroutine.resume(co, "r")) -- true 11 -9
 print("main", coroutine.resume(co, "x", "y")) -- true 10 end
 print("main", coroutine.resume(co, "x", "y")) -- cannot resume dead coroutine
---resume将主协程数据传入次协程， yield将次协程中数据传回主协程
+--resume將主協程數據傳入次協程， yield將次協程中數據傳回主協程
 
 
 ---------------------------------------------------------------------------------
 --[[
-	元表(Metatable)：本质上来说就是存放元方法的表结构， 通过元表实现对表中数据和行为
-	的改变。
-	Lua 查找一个表元素时的规则，其实就是如下 3 个步骤:
-		1.在表中查找，如果找到，返回该元素，找不到则继续
-		2.判断该表是否有元表，如果没有元表，返回 nil，有元表则继续。
-		3.判断元表有没有 __index 方法，如果 __index 方法为 nil，则返回 nil；如果
-		__index 方法是一个表，则重复 1、2、3；如果 __index 方法是一个函数，则返
-		回该函数的返回值
+	元表(Metatable)：本質上來說就是存放元方法的表結構， 通過元表實現對表中數據和行為
+	的改變。
+	Lua 查找一個表元素時的規則，其實就是如下 3 個步驟:
+		1.在表中查找，如果找到，返回該元素，找不到則繼續
+		2.判斷該表是否有元表，如果沒有元表，返回 nil，有元表則繼續。
+		3.判斷元表有沒有 __index 方法，如果 __index 方法為 nil，則返回 nil；如果
+		__index 方法是一個表，則重複 1、2、3；如果 __index 方法是一個函數，則返
+		回該函數的返回值
 	
 --]]
 ---------------------------------------------------------------------------------
@@ -272,12 +272,12 @@ son.otherwork()
 
 ---------------------------------------------------------------------------------
 --[[
-    面向对象：因为lua本身不是面向对象的语言， 在lua中， 通过table和function来模拟一个对象， 
-    用metatable来模拟面向对象中的继承，但是在使用的时候需要考虑lua作为脚本语言， 变量的类型随
-    所赋值类型而改变。
+    面向對像：因為lua本身不是面向對象的語言， 在lua中， 通過table和function來模擬一個對象， 
+    用metatable來模擬面向對像中的繼承，但是在使用的時候需要考慮lua作為腳本語言， 變量的類型隨
+    所賦值類型而改變。
 --]]
 ---------------------------------------------------------------------------------
---父类
+--父類
 rect = {
     area = 0,
     length = 0, 
@@ -302,7 +302,7 @@ function rect:new(leng, wid)
     return self    
 end
 
---子类
+--子類
 cuboid = {
     volume = 0,
     height = 0,
@@ -327,33 +327,33 @@ print("rect1 rectangle:", rect1:getArea())
 
 cuboid1 = cuboid:new(rect1, 2)
 print("cuboid1 volume: ", cuboid1:getVolume())
-print("cuboid1 rectangle: ", cuboid1:getArea())             --子类调用父类方法getArea
-print("cuboid1 length function: ", cuboid1:getLength())     --子类调用父类方法getLength
-print("cuboid1 length variable: ", cuboid1.length)          --子类使用父类变量length
+print("cuboid1 rectangle: ", cuboid1:getArea())             --子類調用父類方法getArea
+print("cuboid1 length function: ", cuboid1:getLength())     --子類調用父類方法getLength
+print("cuboid1 length variable: ", cuboid1.length)          --子類使用父類變量length
 
---重写子类接口getArea， lua中没有重载
+--重寫子類接口getArea， lua中沒有重載
 function cuboid:getArea()                                   
     return 2 * (self.height * self.length + self.height * self.width + self.length * self.width)
 end
 
 cuboid2 = cuboid:new(rect1, 2)
-print("cuboid2 function: getArea: ", cuboid2:getArea())                     --调用子类重写的方法getArea
-print("cuboid2 base function: getArea: ", getmetatable(cuboid2):getArea())  --显示调用父类方法getArea
+print("cuboid2 function: getArea: ", cuboid2:getArea())                     --調用子類重寫的方法getArea
+print("cuboid2 base function: getArea: ", getmetatable(cuboid2):getArea())  --顯示調用父類方法getArea
 
 
 ---------------------------------------------------------------------------------
 --[[
-	模块与C包: 模块类似封装库， 有利于代码复用， 降低耦合， 提供被调用的API。
+	模塊與C包: 模塊類似封裝庫， 有利於代碼復用， 降低耦合， 提供被調用的API。
 	----------------------------------------------------------------------
-	-- 文件名为 module.lua, 定义一个名为 module 的模块
+	-- 文件名為 module.lua, 定義一個名為 module 的模塊
 	module = {}
-	module.constant = "这是一个常量"
+	module.constant = "這是一個常量"
 	function module.func1()
-		io.write("这是一个公有函数！\n")
+		io.write("這是一個公有函數！\n")
 	end
 	
 	local function func2()
-		print("这是一个私有函数！")
+		print("這是一個私有函數！")
 	end
 	
 	function module.func3()
@@ -362,12 +362,12 @@ print("cuboid2 base function: getArea: ", getmetatable(cuboid2):getArea())  --�
  
 	return module
 	
-	在其他模块中调用module模块：
+	在其他模塊中調用module模塊：
 	local m = require("module")
 	print(m.constant)
 	----------------------------------------------------------------------
-	与Lua中写包不同，C包在使用以前必须首先加载并连接，在大多数系统中最容易的实现方式
-	是通过动态连接库机制。Lua在一个叫loadlib的函数内提供了所有的动态连接的功能。
+	與Lua中寫包不同，C包在使用以前必須首先加載並連接，在大多數系統中最容易的實現方式
+	是通過動態連接庫機制。Lua在一個叫loadlib的函數內提供了所有的動態連接的功能。
 	
 	----------------------------------------------------------------------
 --]]
@@ -376,28 +376,28 @@ print("cuboid2 base function: getArea: ", getmetatable(cuboid2):getArea())  --�
 
 ---------------------------------------------------------------------------------
 --[[
-	lua标准库： 标准库中接口可直接使用不需要require
-	常用标准库：
-		math		数学计算
-		table		表结构数据处理
-		string		字符串处理
-		os			系统库函数
-		io			文件读写
-		coroutine	协程库
-		debug		调式器
+	lua標準庫： 標準庫中接口可直接使用不需要require
+	常用標準庫：
+		math		數學計算
+		table		表結構數據處理
+		string		字符串處理
+		os			系統庫函數
+		io			文件讀寫
+		coroutine	協程庫
+		debug		調式器
 --]]
 ---------------------------------------------------------------------------------
 
 
 ---------------------------------------------------------------------------------
 --[[
-	lua虚拟机：脚本语言没有像编译型语言那样直接编译为机器能识别的机器代码，这意味着
-	解释性脚本语言与编译型语言的区别：由于每个脚本语言都有自己的一套字节码，与具体的
-	硬件平台无关，所以无需修改脚本代码，就能运行在各个平台上。硬件、软件平台的差异都
-	由语言自身的虚拟机解决。由于脚本语言的字节码需要由虚拟机执行，而不像机器代码那样
-	能够直接执行，所以运行速度比编译型语言差不少。有了虚拟机这个中间层，同样的代码可
-	以不经修改就运行在不同的操作系统、硬件平台上。Java、Python都是基于虚拟机的编程语
-	言，Lua同样也是这样。
+	lua虛擬機：腳本語言沒有像編譯型語言那樣直接編譯為機器能識別的機器代碼，這意味著
+	解釋性腳本語言與編譯型語言的區別：由於每個腳本語言都有自己的一套字節碼，與具體的
+	硬件平台無關，所以無需修改腳本代碼，就能運行在各個平台上。硬件、軟件平台的差異都
+	由語言自身的虛擬機解決。由於腳本語言的字節碼需要由虛擬機執行，而不像機器代碼那樣
+	能夠直接執行，所以運行速度比編譯型語言差不少。有了虛擬機這個中間層，同樣的代碼可
+	以不經修改就運行在不同的操作系統、硬件平台上。Java、Python都是基於虛擬機的編程語
+	言，Lua同樣也是這樣。
 
 
 --]]
@@ -406,4 +406,4 @@ print("cuboid2 base function: getArea: ", getmetatable(cuboid2):getArea())  --�
 
 
 
---可在命令行lua lua.lua运行本脚本
+--可在命令行lua lua.lua運行本腳本
